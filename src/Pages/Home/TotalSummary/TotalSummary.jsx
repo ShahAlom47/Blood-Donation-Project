48878edfee629891b2ee03a48b0aa7feb220183f
sah alom <sahalom4729@gmail.com>
2024-06-28 11:32:37 +0400

    update summary section

--- src/Pages/Home/TotalSummary/TotalSummary.jsx
+++ src/Pages/Home/TotalSummary/TotalSummary.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import CountUp from 'react-countup';
 import { useInView } from 'react-intersection-observer';
 import img from '../../../assets/image/total-sec.jpg';
-import { HiOutlineCheckBadge } from 'react-icons/hi2';
+import medal from '../../../assets/png/medal.png'
+import client from '../../../assets/png/costumer.png'
+import donar from '../../../assets/png/stethoscope.png'
+import award from '../../../assets/png/trophy.png'
 
 const TotalSummary = () => {
     const { ref, inView } = useInView({
-        triggerOnce: true,
+        // triggerOnce: true,
         threshold: 0.3,
     });
 
@@ -16,24 +19,27 @@ const TotalSummary = () => {
             className="relative flex items-center justify-center py-12 bg-cover bg-center"
             style={{ backgroundImage: `url(${img})` }}
         >
-            <div className="absolute inset-0 bg-black opacity-70"></div>
+            <div className="absolute inset-0 bg-black opacity-90"></div>
             <div className="relative text-center text-white max-w">
-                <div className="flex justify-around gap-8 mt-8">
-                    <div>
-                        <HiOutlineCheckBadge />
-                        {inView && <CountUp end={5} duration={3} className="text-xl" />}
+                <div className="flex justify-around flex-wrap gap-8 mt-8">
+                    <div className=' flex flex-col items-center justify-center gap-5'>
+                       <img src={medal} alt="" className="w-20 bg-color-p p-4 rounded-b-full" />
+                        {inView && <CountUp end={5} duration={3} className="text-5xl font-bold " />}
                         <h3 className="text-xl font-semibold">YEAR EXPERIENCE</h3>
                     </div>
-                    <div>
-                        {inView && <CountUp end={500} duration={3} className="text-xl" />}
-                        <h3 className="text-xl font-semibold">HAPPY DONORS</h3>
-                    </div>
-                    <div>
-                        {inView && <CountUp end={20} duration={3} className="text-xl" />}
+                    <div className=' flex flex-col items-center justify-center gap-5'>
+                       <img src={award} alt="" className="w-20 bg-color-p p-4 rounded-b-full" />
+                        {inView && <CountUp end={5} duration={3} className="text-5xl font-bold " />}
                         <h3 className="text-xl font-semibold">TOTAL AWARDS</h3>
                     </div>
-                    <div>
-                        {inView && <CountUp end={400} duration={3} className="text-xl" />}
+                    <div className=' flex flex-col items-center justify-center gap-5'>
+                       <img src={donar} alt="" className="w-20 bg-color-p p-4 rounded-b-full" />
+                        {inView && <CountUp end={300} duration={3} className="text-5xl font-bold " />}
+                        <h3 className="text-xl font-semibold">HAPPY DONORS</h3>
+                    </div>
+                    <div className=' flex flex-col items-center justify-center gap-5'>
+                       <img src={client} alt="" className="w-20 bg-color-p p-4 rounded-b-full" />
+                        {inView && <CountUp end={450} duration={3} className="text-5xl font-bold " />}
                         <h3 className="text-xl font-semibold">HAPPY RECIPIENT</h3>
                     </div>
                 </div>
