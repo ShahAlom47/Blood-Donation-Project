a32b8a8b6df61f4e1b546f1d701a522a05fdc79e
sah alom <sahalom4729@gmail.com>
2024-07-24 14:26:14 +0400

    added email js for contact form

--- src/SharedComponent/Navbar.jsx
+++ src/SharedComponent/Navbar.jsx
@@ -48,10 +48,10 @@ const Navbar = () => {
 
 
     const nav = <>
-        <NavLink className={`hover:underline animate__animated animate__bounceInUp `}>Home</NavLink>
-        <NavLink className={`hover:underline animate__animated animate__bounceInUp`}>About Us</NavLink>
-        <NavLink className={`hover:underline animate__animated animate__bounceInUp`}>Campaign</NavLink>
-        <NavLink className={`hover:underline animate__animated animate__bounceInUp`}>Blog</NavLink>
+        <NavLink className={`hover:underline px-3 rounded-sm animate__animated animate__bounceInUp `}>Home</NavLink>
+        <NavLink className={`hover:underline px-3 rounded-sm animate__animated animate__bounceInUp`}>About Us</NavLink>
+        <NavLink className={`hover:underline px-3 rounded-sm animate__animated animate__bounceInUp`}>Campaign</NavLink>
+        <NavLink className={`hover:underline px-3 rounded-sm animate__animated animate__bounceInUp`}>Blog</NavLink>
         <NavLink to={'/contact'} className={`hover:underline animate__animated animate__bounceInUp`}>Contact Us</NavLink>
         <label onClick={themeControl} className="flex cursor-pointer gap-2 items-center ml-3 hover:text-black">
 
