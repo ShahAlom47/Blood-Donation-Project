ecfabb449f0a5b1babae99067880c59b5b7aee6a
sah alom <sahalom4729@gmail.com>
2024-07-26 12:40:52 +0400

    update blood requ form

--- src/Components/RequestForm.jsx
+++ src/Components/RequestForm.jsx
@@ -2,15 +2,25 @@ import { useForm } from "react-hook-form";
 import useUser from "../CustomHocks/useUser";
 import Swal from "sweetalert2";
 import { useLocation, useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import DatePicker from "react-datepicker";
 
 const RequestForm = () => {
     const { user } = useUser();
     const navigate = useNavigate();
     const location = useLocation();
+    const [requireDate, setRequireDate] = useState(null);
     const { register, handleSubmit, reset } = useForm();
 
-   
+    const handelDate = (date) => {
+        setRequireDate(date)
+    }
+    const customStyles = {
+        menu: (provided) => ({
+            ...provided,
+            zIndex: 9999
+        })
+    };
     useEffect(() => {
         const formData = JSON.parse(localStorage.getItem('RequFormData'));
         if (formData) {
@@ -36,8 +46,16 @@ const RequestForm = () => {
             return;
         }
 
-        console.log(data);
         localStorage.removeItem('RequFormData');
+
+        const formData = {
+            ...data,
+            requestedDate: new Date().toLocaleDateString('en-GB'),
+            requireDate: requireDate ? requireDate.toLocaleDateString('en-GB') : null,
+            status: 'In Progress'
+
+        }
+        console.log(formData);
     };
 
     return (
@@ -59,6 +77,17 @@ const RequestForm = () => {
                     <option value="O+" className="font-bold">O(+)</option>
                     <option value="O-" className="font-bold">O(-)</option>
                 </select>
+                <div className="mb-4 ">
+                    <div className="bg-gray-300 rounded-sm py-2 px-4 w-full outline-none">
+                        <DatePicker
+                            placeholderText='Required Date'
+                            selected={requireDate}
+                            onChange={handelDate}
+                            className="w-full mt-1 bg-transparent outline-none"
+                            styles={customStyles}
+                        />
+                    </div>
+                </div>
                 <textarea placeholder="Your Message" className="bg-gray-300 rounded-sm py-2 px-4 w-full outline-none" {...register("message")} required></textarea>
 
                 <button type="submit" className="btn-p cursor-pointer">Submit</button>
