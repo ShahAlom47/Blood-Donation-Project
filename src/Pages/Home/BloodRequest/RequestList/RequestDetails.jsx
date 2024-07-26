186557302bb664e30ceeba6dc6a8fec24071ec04
sah alom <sahalom4729@gmail.com>
2024-07-26 20:40:19 +0400

    added details data fetech functionality

--- src/Pages/Home/BloodRequest/RequestList/RequestDetails.jsx
+++ src/Pages/Home/BloodRequest/RequestList/RequestDetails.jsx
@@ -17,14 +17,14 @@ const RequestDetails = () => {
             return res.data;
         }
     });
-
+console.log(data);
     return (
         <div>
             <PageHeading title={'Request Details'} img={bg}></PageHeading>
-            <div className="">
+            <div className="max-w lg:p-10 md:p-8  p-2">
                 {
-                isLoading?<Loading></Loading>:<div>
-
+                isLoading?<Loading></Loading>:<div className="lg:w-8/12 md:w-10/12 w-full m-auto rounded-sm lg:p-10 md:p-8  p-2 border-2 shadow-xl">
+                    <h1 className="text-2xl">{data.address}</h1>
                 </div>
                 }
                 
