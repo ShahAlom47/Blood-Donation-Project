dfd637976b7dafab4796072f3f98b5d7226ba5d2
sah alom <sahalom4729@gmail.com>
2024-08-30 21:41:50 +0400

    fetch user total donation summary

--- src/Pages/DashBoard/Componets/UserComponents/UserDonationSummary.jsx
+++ src/Pages/DashBoard/Componets/UserComponents/UserDonationSummary.jsx
@@ -1,17 +1,34 @@
 
+import { useQuery } from '@tanstack/react-query';
 import { PieChart, Pie, Tooltip, Cell,  } from 'recharts';
+import useAxios from '../../../../CustomHocks/useAxiosSecure';
+import useUser from '../../../../CustomHocks/useUser';
 
 const UserDonationSummary = () => {
+    const {user}=useUser();
+    const AxiosSecure=useAxios();
+
+    const {data}=useQuery({
+        queryKey:'userDonationSummary',
+        queryFn: async()=>{
+            const res= await AxiosSecure.get(`/moneyDonation/userDonationSummary/${user?.email}`)
+            return res.data;
+        }
+    });
+
+    const oneTimeDonation=parseInt(data?.oneTimeDonation);
+    const monthlyDonation=parseInt(data?.monthlyDonation);
+
     
-    const data = [
-        { name: 'Monthly Donation', value: 4000 },
-        { name: 'One-Time Donation', value: 2400 },
+    const chartData = [
+        { name: 'Monthly Donation', value: monthlyDonation },
+        { name: 'One-Time Donation', value: oneTimeDonation },
     ];
 
     const COLORS = ['#0b7127', '#00C49F'];
 
     // Calculate the total amount for percentage calculation
-    const totalAmount = data.reduce((acc, item) => acc + item.value, 0);
+    const totalAmount = chartData.reduce((acc, item) => acc + item.value, 0);
 
     return (
         <div className='grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 bg-gray-400 bg-opacity-10 p-4 my-5 rounded-sm shadow-xl'>
@@ -19,16 +36,16 @@ const UserDonationSummary = () => {
                 <div className=" m-3 mt-0 p-3 bg-opacity-10">
                     <div className=" relative flex justify-center items-center mx-auto p-4 w-56 h-56  rounded-lg">
                         <div className="relative flex justify-center items-center w-10/12 h-5/6 m-auto  border-4 border-color-p rounded-full">
-                            <div className="absolute top-1 flex justify-center text-center items-center">
-                                <span className="font-bold ">Total <br /> Donations</span>
+                            <div className="absolute top-4 flex justify-center text-center items-center">
+                                <span className="font-bold text-2xl ">Total </span>
                             </div>
 
-                            <div className="absolute bottom-3 flex justify-center items-center w-full">
-                                <span className="font-bold ">{2024} Year</span>
+                            <div className="absolute bottom-4 flex justify-center items-center w-full">
+                                <span className="font-bold text-xl ">Donations</span>
                             </div>
                         </div>
                         <div className=" absolute top-[42%] flex flex-col justify-center items-center bg-color-p text-white w-full h-10  rounded-full z-10">
-                            <span className="font-semibold text-xl">2000 TK</span>
+                            <span className="font-semibold text-2xl">{oneTimeDonation+monthlyDonation} TK</span>
                         </div>
                     </div>
                 </div>
@@ -38,7 +55,7 @@ const UserDonationSummary = () => {
                     <div className='flex flex-col justify-center p-4'>
                         <h3 className='text-xl font-semibold mb-2'>Donation Summary</h3>
                         <ul className='list-disc pl-5'>
-                            {data.map((entry, index) => (
+                            {chartData.map((entry, index) => (
                                 <li key={`item-${index}`} className='mb-2'>
                                     <span className='font-semibold text-lg' style={{ color: COLORS[index % COLORS.length] }}>
                                         {entry.name}:
@@ -49,7 +66,7 @@ const UserDonationSummary = () => {
                     </div>
                     <PieChart width={250} height={250}>
                         <Pie
-                            data={data}
+                            data={chartData}
                             cx={125}
                             cy={125}
                             innerRadius={50}
@@ -57,7 +74,7 @@ const UserDonationSummary = () => {
                             paddingAngle={2}
                             dataKey="value"
                         >
-                            {data.map((entry, index) => (
+                            {chartData.map((entry, index) => (
                                 <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                             ))}
 
